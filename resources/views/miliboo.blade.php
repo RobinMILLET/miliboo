7d4bd3bf96745119d1328d8b8072b5b7326022f3
s234 <millerob@etu.univ-smb.fr>
2024-11-25 09:08:11 +0000

    Synchronisation du répertoire distant prêt pour push individuels

---
+++ resources/views/miliboo.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Miliboo</title>
+</head>
+<body>
+    <div class="info-bar container-fluid bg-green-miliboo">
+        <p class="text-center py-2 fs-20">
+            -10% supp avec le code BLACK même sur les promos &amp; Livraison gratuite! <sup>(1)</sup>
+        </p>
+    </div>
+</body>
+</html>
